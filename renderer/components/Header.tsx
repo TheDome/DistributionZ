c27303e8297a497dd8ec8c5b66645380cfa50da7
Dominic Heun <dominic.heun@gmx.net>
2022-09-30 18:53:10 +0200

    fix: make links clickable in UI (#8)
    Fixes an issue where you would not be able to navigate through the app

--- renderer/components/Header.tsx
+++ renderer/components/Header.tsx
@@ -1,16 +1,25 @@
+import Link from "next/link";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">DistributionZ</Navbar.Brand>
+        <Link href={"/"} passHref>
+          <Navbar.Brand>DistributionZ</Navbar.Brand>
+        </Link>
         <Navbar.Toggle aria-controls="navbar" />
         <Navbar.Collapse id="navbar">
           <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/employees">Employees</Nav.Link>
-            <Nav.Link href="/distribution">Distributor</Nav.Link>
+            <Link href={"/"} passHref>
+              <Nav.Link>Home</Nav.Link>
+            </Link>
+            <Link href={"/employees"} passHref>
+              <Nav.Link>Employees</Nav.Link>
+            </Link>
+            <Link href={"/distribution"} passHref>
+              <Nav.Link>Distributor</Nav.Link>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
